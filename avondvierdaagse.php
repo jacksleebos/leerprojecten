b02b25d27c74569854403947fb54cb381cf804bc
CodeGorilla <you@example.com>
2019-01-29 14:00:51 +0100

    some skilltest

---
+++ avondvierdaagse.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<h2>Inschrijfformulier vierdaagse</h2>
+
+<div 
+  
+    class="container">
+      <h1>schrijf u in</h1>
+      <p>Schrijf u in om een account aan te maken.</p>
+      <label for="name"><b>Je naam</b></label>
+      <input type="text" placeholder="Enter name" name="naam" required>
+
+      <label for="email"><b>Email adres</b></label>
+      <input type="text" placeholder="Enter Email" name="email" required>>
+
+      <label for="afstand"><b>afstand 5 , 10 , 15</b></label>
+      <input type="text" placeholder="Enter afstand" name="afstand" required>
+
+</body>
+  </form>
+</div>
+
+</body>
+</html>
